cc0736709589bc1f215c58974653f87e17fcb608
Chengyuan Yan <yanchengyuan@gmail.com>
2018-03-20 07:32:08 -0700

    Added FragmentDemo
    FragmentDemo shows how to swipe among fragments using ViewPager

---
+++ FragmentDemo/app/src/main/java/com/lonelycorn/fragmentdemo/MainActivity.java
@@ -0,0 +1,87 @@
+package com.lonelycorn.fragmentdemo;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.ViewGroup;
+
+public class MainActivity extends FragmentActivity {
+    private static final String TAG = "MainActivity";
+    /**
+     * The number of pages (wizard steps) to show in this demo.
+     */
+    private static final int NUM_PAGES = 2;
+
+    /**
+     * The pager widget, which handles animation and allows swiping horizontally to access previous
+     * and next wizard steps.
+     */
+    private ViewPager mPager;
+
+    /**
+     * The pager adapter, which provides the pages to the view pager widget.
+     */
+    private PagerAdapter mPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Instantiate a ViewPager and a PagerAdapter.
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+    }
+
+    /*
+    @Override
+    public void onBackPressed() {
+        if (mPager.getCurrentItem() == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed();
+        } else {
+            // Otherwise, select the previous step.
+            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+        }
+    }
+    */
+
+    /**
+     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
+     * sequence.
+     */
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Log.i(TAG, String.format("getItem: %d", position));
+            switch (position) {
+                case 0: return new FirstFragment();
+                case 1: return new SecondFragment();
+                default:
+                    Log.e(TAG, String.format("invalid position: %d", position));
+                    return new FirstFragment();
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+
+        @Override
+        public void setPrimaryItem(ViewGroup container, int position, Object object) {
+            Log.i(TAG, String.format("setPrimaryItem: %d", position));
+            super.setPrimaryItem(container,  position, object);
+        }
+    }
+}
---
+++ FragmentDemo/app/src/main/java/com/lonelycorn/fragmentdemo/SecondFragment.java
@@ -0,0 +1,102 @@
+package com.lonelycorn.fragmentdemo;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.util.Log;
+
+/**
+ * Created by cyan on 3/20/18.
+ */
+
+public class SecondFragment extends Fragment {
+    private static final String TAG = "SecondFragment";
+
+    @Override
+    public void onAttach(Context context) {
+        Log.i(TAG, "onAttach");
+        super.onAttach(context);
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate");
+        super.onCreate(savedInstanceState);
+    }
+
+
+    // called when re-created from the backstack <-- what is this?
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        Log.i(TAG, "onCreateView");
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_second, container, false);
+
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstance) {
+        Log.i(TAG, "onActivityCreated");
+        super.onActivityCreated(savedInstance);
+    }
+
+    @Override
+    public void onStart() {
+        Log.i(TAG, "onStart");
+        super.onStart();
+    }
+
+    // now it becomes visible
+
+
+    @Override
+    public void onResume() {
+        Log.i(TAG, "onResume");
+        super.onResume();
+    }
+
+    // now it becomes active
+
+    // DO STUFF HERE
+
+    // now it is no longer active
+
+    @Override
+    public void onPause() {
+        Log.i(TAG, "onPause");
+        super.onPause();
+    }
+
+    // no it is no longer visible
+
+    @Override
+    public void onStop() {
+        Log.i(TAG, "onStop");
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroyView() {
+        Log.i(TAG, "onDestroyView");
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i(TAG, "onDestroy");
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDetach() {
+        Log.i(TAG, "onDetach");
+        super.onDetach();
+    }
+}
